4bfd39f96fed66584a9ec668d4e080c3300254bc
wooseong-choi <ghkdgo8686@gmail.com>
2024-04-27 00:22:12 +0900

    틀린그림 찾기를 통해 오류를 찾았다.

--- mm.c
+++ mm.c
@@ -36,13 +36,13 @@ team_t team = {
 };
 
 /* single word (4) or double word (8) alignment */
-#define ALIGNMENT 8
+// #define ALIGNMENT 8
 
 /* rounds up to the nearest multiple of ALIGNMENT */
-#define ALIGN(size) (((size) + (ALIGNMENT-1)) & ~0x7)
+// #define ALIGN(size) (((size) + (ALIGNMENT-1)) & ~0x7)
 
 
-#define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
+// #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
 
 /* 기본 상수 및 매크로 */
 #define WSIZE 4 /* Word 및 header/footer 크기(바이트) */
@@ -67,16 +67,18 @@ team_t team = {
 #define FTRP(bp) ((char *)(bp) + GET_SIZE(HDRP(bp)) - DSIZE)
 
 /* 블록 ptr bp가 주어지면 다음 블록과 이전 블록의 주소를 계산합니다 */
-#define NEXT_BLKP(bp) ((char *)(bp) + GET_SIZE(((char *)(bp) - WSIZE)))
-#define PREV_BLKP(bp) ((char *)(bp) - GET_SIZE(((char *)(bp) - DSIZE)))
+#define NEXT_BLKP(bp) (((char *)(bp) + GET_SIZE((char *)(bp) - WSIZE)))
+#define PREV_BLKP(bp) (((char *)(bp) - GET_SIZE((char *)(bp) - DSIZE)))
 
-static char *heap_listp;
+void *heap_listp;
 static void *coalesce(void *bp);
-static char *find_fit( size_t asize );
+static void *find_fit( size_t asize );
 static void place(void *bp, size_t asize);
+static void *extend_heap(size_t words);
 
 static void *extend_heap(size_t words)
 {
+    // printf("extend_heap\n");
     char *bp; // 블록 포인터 생성
     size_t size;
 
@@ -96,6 +98,7 @@ static void *extend_heap(size_t words)
 
 static void *coalesce(void *bp)
 {
+    // printf("coalesce\n");
     size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp))); // 이전 블록의 크기 반환 
     size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp))); // 다음 블록의 크기 반환
     size_t size = GET_SIZE(HDRP(bp)); // 현재 블록의 크기 반환
@@ -134,14 +137,16 @@ static void *coalesce(void *bp)
  */
 int mm_init(void)
 {   
+    // printf("init\n");
     // printf("^_^^_^^_^^_^^_^^_^^_^");
     /*
         할당기는 힙을 하나의 커다란 바이트 배열과, 이 배열의 첫 바이트를 가리키는 포인터 p로 구성할 수 있다.
         size 바이트를 할당하기 위해서 malloc은 현재 p 값을 스택에 저장하고, p를 size 만큼 증가시키며, p의 이전 값을 호출자에게 리턴한다.
     */
     /* 초기화된 빈 힙 생성 */
-    if ((heap_listp = mem_sbrk(4*WSIZE)) == (void *)-1) 
+    if ((heap_listp = mem_sbrk(4*WSIZE)) == (void *)-1) {
         return -1;
+    }
     PUT(heap_listp, 0); // 초기 패딩 삽입
     PUT(heap_listp + (1*WSIZE), PACK(DSIZE, 1)); // 프롤로그 블록의 헤더 삽입
     PUT(heap_listp + (2*WSIZE), PACK(DSIZE, 1));  // 프롤로그 블록의 푸터 삽입
@@ -151,8 +156,9 @@ int mm_init(void)
     heap_listp += (2*WSIZE); // 힙 가용 리스트 포인터의 위치를 프롤로그 블록과 에필로그 블록의 사이로 한다.
 
     /* 청크 사이즈의 가용 블록으로 빈 힙을 확장한다. */
-    if(extend_heap(CHUNKSIZE/WSIZE) == NULL)
+    if(extend_heap(CHUNKSIZE/WSIZE) == NULL){
         return -1;
+    }
     
     return 0;
 }
@@ -163,13 +169,15 @@ int mm_init(void)
  */
 void *mm_malloc(size_t size)
 {
+    // printf("malloc\n");
     size_t asize; /* 조정된 블록 사이즈 */ 
     size_t extendsize; /* 적합하지 않을 때 힙을 확장할 용량  */
     char *bp; // 블록 포인터
 
     /* 유효하지 않은 요청 무시 */    
-    if(size == 0)
+    if(size == 0){
         return NULL;
+    }
     
     /* 오버헤드 및 정렬 요구 사항을 포함하도록 블록 크기를 조정합니다. */
     if(size <= DSIZE){
@@ -186,15 +194,17 @@ void *mm_malloc(size_t size)
 
     /* 적합한 가용 용량이 없을 때 메모리 확보 후 데이터 배치한다. */
     extendsize = MAX(asize, CHUNKSIZE);
-    if((bp = extend_heap(extendsize/WSIZE)) == NULL)
+    if((bp = extend_heap(extendsize/WSIZE)) == NULL){
         return NULL;
+    }
     place(bp, asize);
     return bp;
 
 }
 
-static char *find_fit( size_t asize )
+static void *find_fit( size_t asize )
 {   
+    // printf("find fit");
     // char *temp = heap_listp; // 템프 받음
     // 맨땅 코드 
     // while (NEXT_BLKP(temp) != NULL && GET_SIZE(NEXT_BLKP(temp)) != 0) // 다음블록이 없거나, 다음 블록의 사이즈가 0일때 == 에필로그 블록일 경우
@@ -214,7 +224,7 @@ static char *find_fit( size_t asize )
     // 1차 최적화-답지
     // 분기가 적어서? 
     void *bp;
-    for (bp = heap_listp; GET_SIZE(HDRP(bp)) > 0; bp = NEXT_BLKP(bp)){
+    for (bp = (char*)heap_listp; GET_SIZE(HDRP(bp)) > 0; bp = NEXT_BLKP(bp)){
         if(!GET_ALLOC(HDRP(bp)) && (asize <= GET_SIZE(HDRP(bp)))){
             return bp;
         }
@@ -225,17 +235,18 @@ static char *find_fit( size_t asize )
 
 static void place( void *bp, size_t asize )
 {
+    // printf("place\n");
     size_t csize = GET_SIZE(HDRP(bp)); // 현재 빈 공간의 크기 저장
 
     if((csize - asize) >= (2*DSIZE)){ // 현재 빈 공간에서 할당할 공간을 뺀 합이 16 바이트 보다 크다면
         PUT(HDRP(bp), PACK(asize,1)); // 데이터 공간 할당
         PUT(FTRP(bp), PACK(asize,1)); // 데이터 공간 할당
         bp = NEXT_BLKP(bp); // 다음 블록으로 이동
-        PUT(HDRP(bp), PACK(csize-asize,1)); // 이동한 블럭의 헤더를 설정해 준다.
-        PUT(FTRP(bp), PACK(csize-asize,1)); // 푸터를설정해 주어 가용 데이터 블록을 분할한다.
+        PUT(HDRP(bp), PACK(csize-asize,0)); // 이동한 블럭의 헤더를 설정해 준다.
+        PUT(FTRP(bp), PACK(csize-asize,0)); // 푸터를설정해 주어 가용 데이터 블록을 분할한다.
     }else{ // 16바이트 보다 작다면 그냥 할당
-        PUT(HDRP(bp), PACK(asize,1));
-        PUT(FTRP(bp), PACK(asize,1));
+        PUT(HDRP(bp), PACK(csize,1));
+        PUT(FTRP(bp), PACK(csize,1));
     }
 }
 
@@ -243,7 +254,8 @@ static void place( void *bp, size_t asize )
  * mm_free - Freeing a block does nothing.
  */
 void mm_free(void *ptr)
-{
+{   
+    // printf("free\n");
     size_t size = GET_SIZE(HDRP(ptr)); // 블록 포인터의 헤더에서 크기정보와 주소를 읽는다.
 
     PUT(HDRP(ptr), PACK(size,0)); // 해당 블록 포인터의 헤더와 푸터를 할당 해제 해준다.
@@ -256,16 +268,19 @@ void mm_free(void *ptr)
  */
 void *mm_realloc(void *ptr, size_t size)
 {
+    // printf("realloc\n");
     void *oldptr = ptr;
     void *newptr;
     size_t copySize;
     
     newptr = mm_malloc(size);
-    if (newptr == NULL)
+    if (newptr == NULL){
       return NULL;
+    }
     copySize = GET_SIZE(HDRP(oldptr));
-    if (size < copySize)
+    if (size < copySize){
       copySize = size;
+    }
     memcpy(newptr, oldptr, copySize);// 메모리의 특정한 부분으로부터 얼마까지의 부분을 다른 메모리 영역으로 복사해주는 함수(old_bp로부터 copySize만큼의 문자를 new_bp로 복사해라)
     mm_free(oldptr);
     return newptr;
